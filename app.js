abf8fa8397e0cf2e818ced424e2f02b83253d787
jimlikesjazz <jim@jimwynn.co.uk>
2022-06-21 16:34:04 +0100

    added cors and changes in libs

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 import express from "express";
 import notesRouter from "./router/notes.js";
+import cors from "cors";
 
+const app = express();
+const port = 3001;
 
-const app = express()
-const port= 3000
+app.use(cors());
+app.use(express.json());
 
+app.use("/notes", notesRouter);
 
-app.use(express.json())
-
-app.use ('/notes', notesRouter)
-
-app.listen(port,()=>{
-    console.log(`example app listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`example app listening on port ${port}`);
+});
--- libs/notesSample.js
+++ libs/notesSample.js
@@ -1,3 +1,6 @@
-const notesSampledata = [{ week: "Week1", day: "Day1", post: "Test data" }];
+const notesSampledata = [
+  { week: "week1", day: "Tue", post: "Test data" },
+  { week: "week11", day: "Wed", post: "Test data" },
+];
 
 export default notesSampledata;
