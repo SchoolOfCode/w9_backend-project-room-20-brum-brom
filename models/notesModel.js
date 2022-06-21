0156dbc56969e21c2a6c16ba120b89a8693301f7
fiona <fiona.kitchen@yahoo.co.uk>
2022-06-21 11:53:10 +0100

    first get up and running

--- models/notesModel.js
+++ models/notesModel.js
@@ -3,7 +3,7 @@ import { query } from '../db/index.js'
 export async function getNotes(){
     const res = await query(`SELECT * FROM notes;`);
     const notesArray = res.rows;
-    res.json(notesArray)
+    return notesArray
     }
 
 
--- router/notes.js
+++ router/notes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { getNotes } from '../models'
+import { getNotes } from '../models/notesModel.js'
 
 const notesRouter = express.Router()
 
-notesRouter.get('/', getNotes)
+notesRouter.get('/', async function(req, res){
+    let result = await getNotes()
+    return res.json({
+        sucess:true,
+        payload: result})
+})
+
+//post/delete/patch/put
 
 
 export default notesRouter
