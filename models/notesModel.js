3ffa5e6e0018c606965032f3ebd67830b9989d32
ArianMoossavi <arianmoossavi@live.co.uk>
2022-06-21 12:20:37 +0100

    post router started

--- models/notesModel.js
+++ models/notesModel.js
@@ -1,9 +1,17 @@
-import { query } from '../db/index.js'
+import { query } from "../db/index.js";
 
-export async function getNotes(){
-    const res = await query(`SELECT * FROM notes;`);
-    const notesArray = res.rows;
-    return notesArray
-    }
+export async function getNotes() {
+  const res = await query(`SELECT * FROM notes;`);
+  const notesArray = res.rows;
+  return notesArray;
+}
 
-
+export async function postNotes(body) {
+  const res = await query(
+    `INSERT INTO notes(week,day,post) VALUES ($1,$2,$3)`,
+    [body.week, body.day, body.post]
+  );
+  console.log(res);
+  const newPost = res.rows;
+  return newPost;
+}
--- router/notes.js
+++ router/notes.js
@@ -1,16 +1,25 @@
 import express from "express";
-import { getNotes } from '../models/notesModel.js'
+import { getNotes, postNotes } from "../models/notesModel.js";
 
-const notesRouter = express.Router()
+const notesRouter = express.Router();
 
-notesRouter.get('/', async function(req, res){
-    let result = await getNotes()
-    return res.json({
-        sucess:true,
-        payload: result})
-})
+notesRouter.get("/", async function (req, res) {
+  let result = await getNotes();
+  return res.json({
+    sucess: true,
+    payload: result,
+  });
+});
+
+notesRouter.post("/", async function (req, res) {
+  const body = req.body;
+  let result = await postNotes(body);
+  return res.json({
+    sucess: true,
+    payload: result,
+  });
+});
 
 //post/delete/patch/put
 
-
-export default notesRouter
+export default notesRouter;
