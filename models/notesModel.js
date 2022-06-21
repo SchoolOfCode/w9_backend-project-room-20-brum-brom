a124b3922667c6f7e3ec3824a754e396a8a10f12
ArianMoossavi <arianmoossavi@live.co.uk>
2022-06-21 15:37:23 +0100

    querys completed

--- models/notesModel.js
+++ models/notesModel.js
@@ -6,10 +6,13 @@ export async function getNotes() {
   return notesArray;
 }
 
-export async function searchNotes( day, week ){
-  const res= await query(`SELECT * FROM notes WHERE day = $1 AND week =$2  RETURNING *;`,[ day, week])
-  const searchedNotes= res.rows
-  return searchedNotes
+export async function searchNotes(day, week) {
+  const res = await query(`SELECT * FROM notes WHERE day = $1 AND week =$2;`, [
+    day,
+    week,
+  ]);
+  const searchedNotes = res.rows;
+  return searchedNotes;
 }
 
 export async function postNotes(newPost) {
@@ -22,35 +25,46 @@ export async function postNotes(newPost) {
   return newPost;
 }
 
-export async function deleteNotesByID(deleteID){
-    const res = await query(`DELETE FROM notes WHERE notes_id = $1 RETURNING *;`, [deleteID])
-    const deletednotes = res.rows;
-    return deletednotes
+export async function deleteNotesByID(deleteID) {
+  const res = await query(
+    `DELETE FROM notes WHERE notes_id = $1 RETURNING *;`,
+    [deleteID]
+  );
+  const deletednotes = res.rows;
+  return deletednotes;
 }
 
-export async function replaceNotesByID(replaceID, replaceNotes){
-    const res = await query(`UPDATE notes SET week = $2, day = $3, post = $4 WHERE notes_id = $1 RETURNING *;`, 
-    [replaceID, replaceNotes.week, replaceNotes.day, replaceNotes.post])
-    const replacednotes = res.rows;
-    return replacednotes;
+export async function replaceNotesByID(replaceID, replaceNotes) {
+  const res = await query(
+    `UPDATE notes SET week = $2, day = $3, post = $4 WHERE notes_id = $1 RETURNING *;`,
+    [replaceID, replaceNotes.week, replaceNotes.day, replaceNotes.post]
+  );
+  const replacednotes = res.rows;
+  return replacednotes;
 }
 
-export async function patchNoteByID(replaceID, patchNotes){
-    let patchedNotes;
-    if(patchNotes.day !== undefined){
-        const res = await query(`UPDATE notes SET day = $2 WHERE notes_id = $1 RETURNING *;`, 
-        [replaceID, patchNotes.day]);
-        patchedNotes = res.rows;
-    }
-    if(patchNotes.week !== undefined){
-        const res = await query(`UPDATE notes SET week = $2 WHERE notes_id = $1 RETURNING *;`, 
-        [replaceID, patchNotes.week]);
-        patchedNotes = res.rows;
-    }
-    if(patchNotes.post !== undefined){
-        const res = await query(`UPDATE notes SET post = $2 WHERE notes_id = $1 RETURNING *;`, 
-        [replaceID, patchNotes.post]);
-        patchedNotes = res.rows;
-    }
-    return patchedNotes;
+export async function patchNoteByID(replaceID, patchNotes) {
+  let patchedNotes;
+  if (patchNotes.day !== undefined) {
+    const res = await query(
+      `UPDATE notes SET day = $2 WHERE notes_id = $1 RETURNING *;`,
+      [replaceID, patchNotes.day]
+    );
+    patchedNotes = res.rows;
+  }
+  if (patchNotes.week !== undefined) {
+    const res = await query(
+      `UPDATE notes SET week = $2 WHERE notes_id = $1 RETURNING *;`,
+      [replaceID, patchNotes.week]
+    );
+    patchedNotes = res.rows;
+  }
+  if (patchNotes.post !== undefined) {
+    const res = await query(
+      `UPDATE notes SET post = $2 WHERE notes_id = $1 RETURNING *;`,
+      [replaceID, patchNotes.post]
+    );
+    patchedNotes = res.rows;
+  }
+  return patchedNotes;
 }
--- router/notes.js
+++ router/notes.js
@@ -1,17 +1,22 @@
 import express from "express";
-import { getNotes, postNotes, deleteNotesByID, replaceNotesByID, patchNoteByID, searchNotes } from "../models/notesModel.js";
+import {
+  getNotes,
+  postNotes,
+  deleteNotesByID,
+  replaceNotesByID,
+  patchNoteByID,
+  searchNotes,
+} from "../models/notesModel.js";
 
 const notesRouter = express.Router();
 
 notesRouter.get("/", async function (req, res) {
-    if(req.query.day!==undefined && req.query.week!==undefined){
-        let day =req.query.day
-        let week =req.query.week
-        let result = await searchNotes(day, week)
-        return res.json({ sucess: true,
-            payload: result})
-        }
-
+  if (req.query.day !== undefined && req.query.week !== undefined) {
+    let day = req.query.day;
+    let week = req.query.week;
+    let result = await searchNotes(day, week);
+    return res.json({ sucess: true, payload: result });
+  }
 
   let result = await getNotes();
   return res.json({
@@ -20,7 +25,7 @@ notesRouter.get("/", async function (req, res) {
   });
 });
 
-//we want req.query.day and rq.query.week 
+//we want req.query.day and rq.query.week
 
 /* notesRouter.get("/", async function(req,res){
     if(req.query.day!==undefined && req.query.week!==undefined){
@@ -35,43 +40,40 @@ notesRouter.post("/", async function (req, res) {
   const newPost = req.body;
   console.log(newPost);
   let result = await postNotes(newPost);
-//   console.log(result);
+  //   console.log(result);
   return res.json({
     sucess: true,
     payload: result,
   });
 });
 
-notesRouter.delete('/:id', async function(req, res){
-    const deleteID= req.params.id;
-    const result = await deleteNotesByID(deleteID);
-    res.json({
-        success :true, 
-        payload:result
-    });
-})
+notesRouter.delete("/:id", async function (req, res) {
+  const deleteID = req.params.id;
+  const result = await deleteNotesByID(deleteID);
+  res.json({
+    success: true,
+    payload: result,
+  });
+});
 
-notesRouter.put('/:id', async function(req, res){
-    const replaceNotes = req.body
-    const replaceID= req.params.id;
-    const result = await replaceNotesByID(replaceID, replaceNotes);
-    res.json({
-        success :true, 
-        payload:result
-    });
-})
+notesRouter.put("/:id", async function (req, res) {
+  const replaceNotes = req.body;
+  const replaceID = req.params.id;
+  const result = await replaceNotesByID(replaceID, replaceNotes);
+  res.json({
+    success: true,
+    payload: result,
+  });
+});
 
-notesRouter.patch('/:id', async function(req, res){
-    const patchNotes = req.body
-    const replaceID= req.params.id;
-    const result = await patchNoteByID(replaceID, patchNotes);
-    res.json({
-        success :true, 
-        payload:result
-    });
-})
-
-
-
+notesRouter.patch("/:id", async function (req, res) {
+  const patchNotes = req.body;
+  const replaceID = req.params.id;
+  const result = await patchNoteByID(replaceID, patchNotes);
+  res.json({
+    success: true,
+    payload: result,
+  });
+});
 
 export default notesRouter;
