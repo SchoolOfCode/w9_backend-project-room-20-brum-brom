44ed03ef6fdbb22af555dbb63377b5b3dc9f76e4
fiona <fiona.kitchen@yahoo.co.uk>
2022-06-21 10:08:54 +0100

    set up pg

---
+++ db/index.js
@@ -0,0 +1,18 @@
+
+import pg from 'pg'
+
+
+
+const connectionString =process.env.PGURL
+
+const pool = new pg.Pool({
+connectionString,
+ssl: { rejectUnauthorized: false }
+})
+
+
+export function query (text, params, callback){
+    return pool.query(text, params, callback)
+  }
+
+console.log(await query('SELECT NOW()'))
