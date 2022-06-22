557049bd8edda4572bc3eb6ba5708966ecb2865c
ArianMoossavi <arianmoossavi@live.co.uk>
2022-06-22 12:14:54 +0100

    table populated with data

--- db/scripts/populate-notes-table.js
+++ db/scripts/populate-notes-table.js
@@ -1,16 +1,15 @@
-import notesSampledata from "../../libs/notesSample.js";
+import emptyArray from "../../libs/notesSample.js";
 import { query } from "../../db/index.js";
 
-for (let i = 0; i < notesSampledata.length; i++) {
+for (let i = 0; i < emptyArray.length; i++) {
   await query(
     "INSERT INTO notes(week,day,post,emoji,reflections) VALUES ($1,$2,$3, $4, $5)",
     [
-      notesSampledata[i].week,
-      notesSampledata[i].day,
-      notesSampledata[i].post,
-      notesSampledata[i].emoji,
-      notesSampledata[i].reflections,
+      emptyArray[i].weeks,
+      emptyArray[i].days,
+      emptyArray[i].post,
+      emptyArray[i].emoji,
+      emptyArray[i].reflections,
     ]
   );
-  console.log(`${notesSampledata[i]} added`);
 }
--- libs/notesSample.js
+++ libs/notesSample.js
@@ -1,25 +1,52 @@
-const notesSampledata = [
-  {
-    week: "week1",
-    day: "Tue",
-    post: "Test data",
-    emoji: "happy",
-    reflections: "test ref 1",
-  },
-  {
-    week: "week11",
-    day: "Wed",
-    post: "Test data 2",
-    emoji: "indifferent",
-    reflections: "test ref 2",
-  },
-  {
-    week: "week16",
-    day: "Thu",
-    post: "Test data 3",
-    emoji: "sad",
-    reflections: "test ref 3",
-  },
-];
+function WeeksAndDays(weeks, days, post, emoji, reflections) {
+  this.weeks = weeks;
+  this.days = days;
+  this.post = post;
+  this.emoji = emoji;
+  this.reflections = reflections;
+}
 
-export default notesSampledata;
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "W-end"];
+
+let weekNumbers = [];
+for (let i = 1; i < 17; i++) {
+  weekNumbers.push("Week" + i);
+}
+
+let emptyArray = [];
+for (let j = 0; j < weekNumbers.length; j++) {
+  for (let k = 0; k < daysOfWeek.length; k++) {
+    emptyArray.push(
+      new WeeksAndDays(weekNumbers[j], daysOfWeek[k], "", "", "")
+    );
+  }
+}
+console.log(emptyArray);
+
+export default emptyArray;
+
+// // const emptyArray = [
+// //   {
+// //     week: "week1",
+// //     day: "Mon",
+// //     post: "Test data",
+// //     emoji: "happy",
+// //     reflections: "test ref 1",
+// //   },
+// //   {
+// //     week: "week1",
+// //     day: "Tue",
+// //     post: "",
+// //     emoji: "",
+// //     reflections: "",
+// //   },
+// //   {
+// //     week: "week16",
+// //     day: "Thu",
+// //     post: "Test data 3",
+// //     emoji: "sad",
+// //     reflections: "test ref 3",
+// //   },
+// // ];
+
+// export default notesSampledata;
